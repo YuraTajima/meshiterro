ccf0cccade1deb6e23b34990d95cd479a91ec4e0
EC2 Default User <ec2-user@ip-172-31-20-242.ap-northeast-1.compute.internal>
2022-12-25 09:42:18 +0000

    [Add]4-11

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2022_12_25_065041) do
   create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
-    t.integer "user_i"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
